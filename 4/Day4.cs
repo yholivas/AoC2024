ad446b768c3dfc08772808b9775aed92636863b7
Daniel Olivas <yholivas@gmail.com>
2024-12-04 21:12:26 -0600

    day 4 pt 2

--- 4/Day4.cs
+++ 4/Day4.cs
@@ -4,7 +4,7 @@ public class Day4 : IRunnable
 {
     private bool IsMatch(string text, int idx, int[] offsets, char[] matchList)
     {
-        for (int i = 0; i < 3; ++i) {
+        for (int i = 0; i < offsets.Length; ++i) {
             int offset = offsets[i];
             char matchChar = matchList[i];
 
@@ -16,16 +16,22 @@ public class Day4 : IRunnable
         return true;
     }
 
+    private int LineLength(string text)
+    {
+        int lineLength = 0;
+        while (text[lineLength] != '\n')
+            lineLength++;
+        lineLength++;
+        return lineLength;
+    }
+
     public int Run(string file)
     {
         int result = 0;
 
         string text = File.ReadAllText($"4/{file}");
 
-        int lineLength = 0;
-        while (text[lineLength] != '\n')
-            lineLength++;
-        lineLength++;
+        int lineLength = LineLength(text);
 
         int[][] offsets = [
             [lineLength, lineLength * 2, lineLength * 3],
@@ -52,10 +58,52 @@ public class Day4 : IRunnable
         return result;
     }
 
+    // possible x-mas:
+    // M.S
+    // .A.
+    // M.S
+    //
+    // S.S
+    // .A.
+    // M.M
+    //
+    // S.M
+    // .A.
+    // S.M
+    //
+    // M.M
+    // .A.
+    // S.S
     public int Run2(string file)
     {
         int result = 0;
+        string text = File.ReadAllText($"4/{file}");
 
+        int lineLength = LineLength(text);
+
+        int[] offsets = [2, lineLength + 1, lineLength * 2, lineLength * 2 + 2];
+
+        char[][] mLists = [
+            ['S', 'A', 'M', 'S'],
+            ['M', 'A', 'S', 'S']
+        ];
+
+        char[][] sLists = [
+            ['S', 'A', 'M', 'M'],
+            ['M', 'A', 'S', 'M']
+        ];
+
+        for (int i = 0; i < text.Length; ++i) {
+            char ch = text[i];
+            if (ch == 'M' || ch == 'S') {
+                char[][] matchLists = ch == 'M' ? mLists : sLists;
+
+                foreach (char[] matchList in matchLists) {
+                    if (IsMatch(text, i, offsets, matchList))
+                        result++;
+                }
+            }
+        }
         return result;
     }
 }
